efb3319a9538ee0c4242157718d908e2c9cb48a3
Rendy <rendyexpayer05@gmail.com>
2025-08-31 12:10:52 +0800

    first commit

---
+++ kalkulator.py
@@ -0,0 +1,6 @@
+angka_1 = float(input("Masukkan angka 1 : "))
+angka_2 = float(input("Masukkan angka 2 : "))
+operand = str(input("Masukkan operandnya: "))
+
+if operand == "+":
+    print(f"hasil: {angka_1} + {angka_2} = { angka_1 + angka_2 }")
