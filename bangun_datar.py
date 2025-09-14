f1cea3a0253512d8c66d10902113d30e2c0c5d78
Rendy <rendyexpayer05@gmail.com>
2025-09-14 22:27:45 +0800

    optimasi

--- bangun_datar.py
+++ bangun_datar.py
@@ -3,11 +3,25 @@ import math
 def luas_lingkaran(radius: float)  -> float:
     return  math.pi * radius ** 2
 
+def luas_persegi_panjang(panjang: float, lebar) -> float:
+    return panjang * lebar
+
+def luas_segitiga(alas: float, tinggi: float) -> float:
+    return 0.5 * alas * tinggi
+
+def luas_layang_layang(diagonal_1: float, diagonal_2: float) -> float:
+    return 0.5 * diagonal_1 * diagonal_2
 
 def main():
-    radius = float(input("Masukkan jari jari (cm): "))
-    hasil = luas_lingkaran(radius)
-    print(f"hasilnya : {hasil:.2f} cm2")
+    print("=== menghitung luas bangun datar ===")
+    print("1.lingkaran")
+    print("2.persegi panjang")
+    print("3.segitiga")
+    print("4.layang layang")
+
+    pilihan = input("Masukkan bangun ruang : (1/2/3/4)")
+
+    
 
 if __name__ == "__main__":
     main()
