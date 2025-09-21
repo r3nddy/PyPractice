60c4d7a06ef7331cb2c7cb0a5a3705a43c3a9a6c
Rendy <rendyexpayer05@gmail.com>
2025-09-21 11:22:35 +0800

    rev 2, add diskon

--- coffe_shop_rev1.py
+++ coffe_shop_rev1.py
@@ -1,4 +1,3 @@
-# Simpan menu dengan variabel terpisah
 espresso_nama = "Espresso"
 espresso_harga = 15000
 
@@ -17,7 +16,7 @@ total_pesanan = 0
 print("=== Daftar Menu ===")
 print(f"1. {espresso_nama} : Rp {espresso_harga}")
 print(f"2. {americano_nama} : Rp {americano_harga}")
-print(f"3. {cappuccino_nama} : Rp {cappuccino_harga}")
+print(f"3. {cappuccino_nama} : Rp {cappuccino_harga} (Promo Diskon 10%!!!)")
 print(f"4. {latte_nama} : Rp {latte_harga}")
 print("0. Selesai")
 
@@ -26,26 +25,35 @@ while True:
 
     if pilihan == 0:
         break
+
     elif pilihan == 1:
         jumlah = int(input("Jumlah pesanan: "))
-        subtotal = espresso_harga * jumlah
-        total_pesanan += subtotal
-        print(f"Anda memesan {jumlah} {espresso_nama} (Rp {subtotal})")
+        harga_pesanan = espresso_harga * jumlah
+        total_pesanan += harga_pesanan
+        print(f"Anda memesan {jumlah} {espresso_nama} (Rp {harga_pesanan})")
+
     elif pilihan == 2:
         jumlah = int(input("Jumlah pesanan: "))
-        subtotal = americano_harga * jumlah
-        total_pesanan += subtotal
-        print(f"Anda memesan {jumlah} {americano_nama} (Rp {subtotal})")
+        harga_pesanan = americano_harga * jumlah
+        total_pesanan += harga_pesanan
+        print(f"Anda memesan {jumlah} {americano_nama} (Rp {harga_pesanan})")
+
     elif pilihan == 3:
         jumlah = int(input("Jumlah pesanan: "))
-        subtotal = cappuccino_harga * jumlah
-        total_pesanan += subtotal
-        print(f"Anda memesan {jumlah} {cappuccino_nama} (Rp {subtotal})")
+        harga_pesanan = cappuccino_harga * jumlah
+
+        #diskon 10%
+        diskon = int(harga_pesanan * 0.1)
+        harga_diskon = harga_pesanan - diskon
+
+        total_pesanan += harga_diskon
+        print(f"Anda memesan {jumlah} {cappuccino_nama} (Rp {harga_diskon}, Anda mendapatkan diskon 10%)")
+
     elif pilihan == 4:
         jumlah = int(input("Jumlah pesanan: "))
-        subtotal = latte_harga * jumlah
-        total_pesanan += subtotal
-        print(f"Anda memesan {jumlah} {latte_nama} (Rp {subtotal})")
+        harga_pesanan = latte_harga * jumlah
+        total_pesanan += harga_pesanan
+        print(f"Anda memesan {jumlah} {latte_nama} (Rp {harga_pesanan})")
     else:
         print("Pilihan tidak valid.")
 
